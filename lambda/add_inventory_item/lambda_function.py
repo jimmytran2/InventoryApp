465496b3331f34ab814b99a4e89d5ba82fd6857d
Jimmy Tran <jtran2@academic.rrc.ca>
2025-04-22 17:14:58 -0500

    Added lambda code

---
+++ lambda/add_inventory_item/lambda_function.py
@@ -0,0 +1,45 @@
+import json
+import boto3
+import uuid
+
+def lambda_handler(event, context):
+    # Parse incoming JSON data
+    try:
+        data = json.loads(event['body'])
+    except KeyError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Bad request. Please provide the data.")
+        }
+
+    # DynamoDB setup
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Inventory')
+
+    # Generate a unique ID
+    unique_id = str(uuid.uuid4())
+
+    # Insert data into DynamoDB
+    try:
+        table.put_item(
+            Item={
+                'id': unique_id,
+                'location': data['location'],
+                'name': data['name'],
+                'description': data['description'],
+                'qty': data['qty'],
+                'price': data['price']
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f"Item with ID {unique_id} added successfully.")
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error adding item: {str(e)}")
+        }
+    
+
+# Comment to check if lambda deploy worked
